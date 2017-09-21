0df8ab088eb14fbb9e5f2d2a1331b031555a13fb
alexxxPopa <alex.popa@mbitcasino.com>
2017-09-21 14:56:46 +0300

    Minor changes

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 
 export default function() {
   return [
-    {title : 'Javascript: The good parts'},
-    {title : 'Thinking in Java' },
-    {title : 'The dark tower' },
-    {title : 'Eloquent javascript' }
+    {title : 'Javascript: The good parts', pages: 101 },
+    {title : 'Thinking in Java', pages: 102 },
+    {title : 'The dark tower' , pages: 103 },
+    {title : 'Eloquent javascript', pages: 104 }
   ];
 }
