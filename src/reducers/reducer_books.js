b373de144d6de3379c9a458ad18c1f8cd735539f
alexxxPopa <alex.popa@mbitcasino.com>
2017-09-21 13:39:08 +0300

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function() {
+  return [
+    {title : 'Javascript: The good parts'},
+    {title : 'Thinking in Java' },
+    {title : 'The dark tower' },
+    {title : 'Eloquent javascript' }
+  ];
+}
